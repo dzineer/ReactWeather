5bde0ecb874155f1d9df5e35f326fe1b95cc9fc1
frank@dzineer.com <frankdd3@gmail.com>
2017-07-05 17:11:49 +0700

    Added top bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -14,15 +14,54 @@ var {Link, IndexLink} = require('react-router');
 //   }
 // });
 
-var Nav = (props) => {
-  return (
-    <div>
-      <h2>Nav component</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-      <IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</IndexLink>
-      <Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
-    </div>
-  );
-}
+var Nav = React.createClass({
+  onSearch: function(e) {
+    e.preventDefault();
 
-module.exports = Nav;
+    var location = this.refs.location.value;
+
+    if(location.length > 0) {
+      this.refs.location.value = '';
+      this.props.onSearch( location );
+    }
+    
+  },
+  render: function() {
+    return (
+      <div className="top-bar">
+
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</IndexLink>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather "/>
+              </li>
+
+              <li>
+                <input type="submit" className="button" value="Get Weather" />
+              </li>
+            </ul>
+          </form>
+        </div>
+
+      </div>
+    );
+  }
+});
+
+module.exports = Nav
